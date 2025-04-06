34f8d2c3f3f92a5971c43c8b4549985816999eb7
HaidarSh <haidarshokor2@gmail.com>
2025-04-06 20:33:17 +0300

    setting up database

---
+++ database/migrations/tenant/2025_04_06_172712_create_addresses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('address_line1');
+            $table->string('address_line2')->nullable();
+            $table->foreignId('country_id')->constrained();
+            $table->foreignId('city_id')->constrained();
+            $table->foreignId('province_id')->constrained();
+            $table->string('postal_code')->nullable();
+            $table->string('complex')->nullable();
+            $table->string('building')->nullable();
+            $table->string('floor')->nullable();
+            $table->string('suite')->nullable();
+            $table->string('unit_number')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
