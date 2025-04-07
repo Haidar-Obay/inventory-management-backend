de124e5b8dd7fca3b1034ec3c8371fad3ab7d8b9
HaidarSh <haidarshokor2@gmail.com>
2025-04-07 21:43:12 +0300

    CustomerModel added

---
+++ app/Models/Customer.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    protected $table = 'customers';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $guarded = ['id'];
+
+    public function customerGroup()
+    {
+        return $this->belongsTo(CustomerGroups::class, 'customer_group_id');
+    }
+
+    public function salesman()
+    {
+        return $this->belongsTo(Salesmen::class, 'salesman_id');
+    }
+
+    public function referBy()
+    {
+        return $this->belongsTo(ReferBy::class, 'refer_by_id');
+    }
+
+    public function paymentTerm()
+    {
+        return $this->belongsTo(PaymentTerms::class, 'payment_term_id');
+    }
+
+    public function primaryPaymentMethod()
+    {
+        return $this->belongsTo(PaymentMethods::class, 'primary_payment_method_id');
+    }
+
+    public function openingCurrency()
+    {
+        return $this->belongsTo(Currency::class, 'opening_currency_id');
+    }
+
+    public function Address()
+    {
+        return $this->hasMany(Address::class, 'id', 'billing_address_id');
+    }
+
+    public function parentCustomer()
+    {
+        return $this->belongsTo(Customer::class, 'parent_customer_id');
+    }
+
+    public function subCustomers()
+    {
+        return $this->hasMany(Customer::class, 'parent_customer_id');
+    }
+}
