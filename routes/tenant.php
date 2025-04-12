9da52e9a515b0ef5bb696bdf21b2f638f86b86b8
HaidarSh <haidarshokor2@gmail.com>
2025-04-12 18:20:02 +0300

    Your commit message

--- routes/tenant.php
+++ routes/tenant.php
@@ -62,6 +62,15 @@ Route::middleware([
         Route::post('/register', [UserManagementController::class, 'registerUser']);
         Route::apiResource('cities', CityController::class);
         Route::post('/logout',  [AuthController::class,'logout']);
+        Route::apiResource('countries', CountryController::class);
+        Route::apiResource('provinces', ProvinceController::class);
+        Route::apiResource('currencies', CurrencyController::class);
+        Route::apiResource('salesmen', SalesmanController::class);
+        Route::apiResource('customer-groups', CustomerGroupController::class);
+        Route::apiResource('customers', CustomerController::class);
+        Route::apiResource('refer-bies', ReferByController::class);
+        Route::apiResource('payment-methods', PaymentMethodController::class);
+
     });
 
 
