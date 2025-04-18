1367e871648b70285bd86971af2ae14752ed0856
HaidarSh <haidarshokor2@gmail.com>
2025-04-18 16:41:44 +0300

    xd

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\TenantUserManagementController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
 
 /*
 |--------------------------------------------------------------------------
