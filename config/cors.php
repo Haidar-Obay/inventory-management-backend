539436fd8cc620775a2ed6c3525dae0698fade97
HaidarSh <haidarshokor2@gmail.com>
2025-05-11 17:59:00 +0300

    CORS problem solved

--- config/cors.php
+++ config/cors.php
@@ -1,27 +1,12 @@
 <?php
 
 return [
-
-    'paths' => ['api/*', 'login'],
-
+    'paths' => ['api/*', 'salesmen', '*'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://tenant_1.app.localhost:3000',
-        'http://tenant_2.app.localhost:3000',
-    ],
-
-    'allowed_origins_patterns' => [
-        '/^http:\/\/.+\.app\.localhost:3000$/',
-    ],
-
+    'allowed_origins' => ['http://tenant_2.app.localhost:3000'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
-
+    'supports_credentials' => true, // Important for cookies/session
 ];
