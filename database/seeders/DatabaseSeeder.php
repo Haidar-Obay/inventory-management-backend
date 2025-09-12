03f7a82f928164c9a1152d31d27528c1ed742501
HaidarSh <haidarshokor2@gmail.com>
2025-09-12 20:02:44 +0300

    working with central domain_2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin User',
+            'email' => 'binbotadmin@brain.com',
+            'password' => bcrypt('br@in_binbot'),
         ]);
 
         $this->call([
