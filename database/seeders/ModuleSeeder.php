ea55284e8867027f479e164695e68c1105da1f96
obay123 <obay.kadour999@yahoo.com>
2025-10-21 18:28:06 +0300

    chaning conenction to media types

--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -130,7 +130,7 @@ class ModuleSeeder extends Seeder
                     // Relations
                     ['name' => 'Associations', 'code' => PageCode::RELATIONS_ASSOCIATIONS->value, 'path' => '/main/relations/associations', 'order' => 90, 'is_public' => false],
                     ['name' => 'Referrers', 'code' => PageCode::RELATIONS_REFERRERS->value, 'path' => '/main/relations/referrers', 'order' => 91, 'is_public' => false],
-                    ['name' => 'Connections', 'code' => PageCode::RELATIONS_CONNECTIONS->value, 'path' => '/main/relations/connections', 'order' => 92, 'is_public' => false],
+                    ['name' => 'Media Types', 'code' => PageCode::RELATIONS_MEDIA_TYPES->value, 'path' => '/main/relations/media-types', 'order' => 92, 'is_public' => false],
                     
                     // Support
                     ['name' => 'Help', 'code' => PageCode::SUPPORT_HELP_PAGE->value, 'path' => '/main/support/help', 'order' => 100, 'is_public' => false],
